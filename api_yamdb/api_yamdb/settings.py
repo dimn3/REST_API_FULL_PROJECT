983bb1f29b07fb6e661b95936bde3d45abd4a23b
Дмитрий Парфенов <parfecto@yandex.ru>
2023-02-06 22:41:53 +0300

    1s

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -73,9 +73,9 @@ DATABASES = {
             'DB_ENGINE',
             default='django.db.backends.postgresql'
         ),
-        'NAME': os.getenv('DB_NAME', default=None),
-        'USER': os.getenv('POSTGRES_USER', default=None),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=None),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='localhost'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
